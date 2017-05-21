4239e18b9494100717f6e22fd9d301eaad649275
Bindu Wavell <bindu@ziaconsulting.com>
2017-05-20 18:06:05 -0600

    Initial maven archetype path filtering module

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ common.java_properties = require('./lib/java-properties');
 common.maven_archetype_file_filtering = require('./lib/maven-archetype-file-filtering');
 common.maven_archetype_generate = require('./lib/maven-archetype-generate');
 common.maven_archetype_metadata = require('./lib/maven-archetype-metadata');
+common.maven_archetype_path_filtering = require('./lib/maven-archetype-path-filtering');
 common.maven_pom = require('./lib/maven-pom');
 common.mem_fs_utils = require('./lib/mem-fs-utils');
 common.prompt_filters = require('./lib/prompt-filters');
