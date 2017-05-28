ab90c63a3809b88d200c19c2657a4ad495292dc2
Bindu Wavell <bindu@ziaconsulting.com>
2017-05-28 15:11:29 -0600

    Remove :color from ci.js commands

--- ci.js
+++ ci.js
@@ -7,9 +7,9 @@ var nodeVersion = String(process.env.TRAVIS_NODE_VERSION);
 var cmd;
 
 if (nodeVersion === '7') {
-  cmd = 'npm run codecov:color';
+  cmd = 'npm run codecov';
 } else {
-  cmd = 'npm run test:color';
+  cmd = 'npm run test';
 }
 
 try {
